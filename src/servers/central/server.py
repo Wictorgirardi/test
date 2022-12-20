48f3292116316a25a836f5bc0fe8b7ba4c66937d
Wictorgirardi <wictor.girardi@gmail.com>
2022-12-20 03:52:13 -0300

    wip

--- src/servers/central/server.py
+++ src/servers/central/server.py
@@ -8,116 +8,76 @@ addresses = []
 def sendCommand(conn, COMMAND):
   conn.send(COMMAND.encode('ascii'))
 
-def show_output(conn):
-  try:
+def show_devices(conn, showAll):
     status = conn.recv(2048).decode('ascii')
     status = json.loads(status)
 
     if status['SPres'] == 'ON' or status['SFum'] == 'ON' or status['SJan'] == 'ON' or status['SPor'] == 'ON' :
       status['AL_BZ'] = 'ON'
 
-    print('Saidas:')
-    print('1) L_01: '+status['L_01'])
-    print('2) L_02: '+status['L_02'])
-    print('3) AC: '+status['AC'])
-    print('4) PR: '+status['PR'])
-    print('5) AL_BZ: '+status['AL_BZ'])
-  except:
-    print('Error getting output')
-
-def get_status(conn):
-    status = conn.recv(2048).decode('ascii')
-    status = json.loads(status)
-
-    if status['SPres'] == 'ON' or status['SFum'] == 'ON' or status['SJan'] == 'ON' or status['SPor'] == 'ON' :
-      status['AL_BZ'] = 'ON'
-
-    print('Saidas:')
-    print('L_01: '+status['L_01']+' L_02: '+status['L_02'])
-    print('AC: '+status['AC'])
-    print('PR: '+status['PR'])
-    print('AL_BZ: '+status['AL_BZ'])
-    print('Sensores:')
-    print('SPres: '+status['SPres'])
-    print('SFum: '+status['SFum'])
-    print('SJan: '+status['SJan']+' SPor: '+status['SPor'])
-    print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(status['Temperatura'], status['Humidade']))
-    print('Total de pessoas nesta sala: '+status['Pessoas'])
-
-def get_sucess(conn):
-  try:
-    response = conn.recv(2048).decode('ascii')
-    if response == 'OK':
-      print('Dispositivo alternado com sucesso!')
-    elif response == 'NOT_OK':
-      print('Houve problema em alternar(ON/OFF) dispositivo!!')
-  except:
-    print('Error getting response')
+    if(showAll == 'true'):
+      print('\n')
+      print('Luz 01: ' +status['L_01'])
+      print('Luz 02: ' +status['L_02'])
+      print('Ar condicionado: '+status['AC'])
+      print('Projetor: '+status['PR'])
+      print('Alarme: '+status['AL_BZ'])
+      print('Sensor presença: ' +status['SPres'])
+      print('Sensor fumaça: ' +status['SFum'])
+      print('Sensor janela: ' +status['SJan']+' SPor: '+status['SPor'])
+      print("Temperatura da sala={0:0.1f} Celsius e humidade={1:0.1f}%".format(status['Temperatura'], status['Humidade']))
+      print('Total de pessoas: ' +status['Pessoas'])
+    else: 
+      print('\n')
+      print('1 - Luz 01: ' +status['L_01'])
+      print('2 - Luz 02: ' +status['L_02'])
+      print('3 - Ar condicionado: ' +status['AC'])
+      print('4 - Projetor: ' +status['PR'])
+      print('5 - Alarme: ' +status['AL_BZ'])
 
 def menu():
     op = 0
+    print('Bem vindo ao trabalho 1 de Fundamentos de Sistemas Embarcados do aluno Wictor Girardi, escolha sua opção abaixo:')
     while op != 3:
-      print('Bem vindo ao trabalho 1 de Fundamentos de Sistemas Embarcados do aluno Wictor Girardi')
       print('1 - Listar todos dispositivos') 
       print('2 - Ativar ou desativar dispositivos disponiveis')
       print('3 - Terminar Execução')
-      op = input('Digite uma opcao: ')
-      if int(op)<1 or int(op)> 3:
+      op = int(input())
+
+      if op not in [1, 2, 3]:
         print('Opção invalida, tente novamente:') 
         menu()
-      if int(op) == 1:
-        room = -1
-        while room > len(addresses) or room < 0:
-          print('------- Listar Estados -------')
-          print('Salas conectadas:')
-          for i in range(len(addresses)):
-            print(f'Sala {i} - IP:{addresses[i]}')
-          room = int(input('Digite o numero da sala desejada '))
-
-        sendCommand(listconn[addresses[room]], f'GET_STATUS')
-        get_status(listconn[addresses[room]])
+      if op == 1:
+        sendCommand(listconn[addresses[0]], f'GET_STATUS')
+        show_devices(listconn[addresses[0]], "true")
         input('Aperte enter para continuar...')
-
-      if int(op) == 2:
-        room = -1
-        while room > len(addresses) or room < 0:
-          print('----- Listar Estados -------')
-          print('Salas conectadas:')
-          for i in range(len(addresses)):
-            print(f'Sala {i} - IP:{addresses[i]}')
-          room = int(input('Digite o numero da sala desejada: '))
-        
+      if op == 2:
         device = -1
         while device < 1 or device > 7:
-          print('-------- Dispositivos --------')
-          sendCommand(listconn[addresses[room]], f'GET_STATUS')
-          show_output(listconn[addresses[room]])
+          print('Listagem de dispositivos:')
+          sendCommand(listconn[addresses[0]], f'GET_STATUS')
+          show_devices(listconn[addresses[0]], "false")
           print('OBS: Escolha o digito 6 para acionar todos os dispositivos (ON) e 7 para desativar(OFF)')
           device = int(input('Digite o numero do dispositivo que deseja alternar entre ON/OFF: '))
           if device == 1:
-            sendCommand(listconn[addresses[room]], f'ON_OFF_L_01')
+            sendCommand(listconn[addresses[0]], f'ON_OFF_L_01')
           elif device == 2:
-            sendCommand(listconn[addresses[room]], f'ON_OFF_L_02')
+            sendCommand(listconn[addresses[0]], f'ON_OFF_L_02')
           elif device == 3:
-            sendCommand(listconn[addresses[room]], f'ON_OFF_AC')
+            sendCommand(listconn[addresses[0]], f'ON_OFF_AC')
           elif device == 4:
-            sendCommand(listconn[addresses[room]], f'ON_OFF_PR')
+            sendCommand(listconn[addresses[0]], f'ON_OFF_PR')
           elif device == 5:
-            sendCommand(listconn[addresses[room]], f'ON_OFF_AL_BZ')
+            sendCommand(listconn[addresses[0]], f'ON_OFF_AL_BZ')
           elif device == 6:
-            sendCommand(listconn[addresses[room]], f'ON_ALL')
+            sendCommand(listconn[addresses[0]], f'ON_ALL')
           elif device == 7:
-            sendCommand(listconn[addresses[room]], f'OFF_ALL')
-          get_sucess(listconn[addresses[room]])
-          print('Redirecionando para o menu. Aguarde...')
-        
-      if int(op) == 3:
+            sendCommand(listconn[addresses[0]], f'OFF_ALL')
+          print('Dispositivo alternado com sucesso!') if listconn[addresses[0]].recv(2048).decode('ascii') == 'OK' else print('Ooops! Algo errado aconteceu, tente novamente.')
+      if op == 3:
         print('Obrigado por utilizar o projeto!')
         quit()
-      
-      else:
-        print('Opção invalida, tente novamente!')
-
+    
 if __name__ == '__main__':
   configfile = open('../../configs/configuracao_sala_02.json')
   file = json.load(configfile)
