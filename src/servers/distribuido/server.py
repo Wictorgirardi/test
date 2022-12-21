149f0f7aba8b70002d27095482d34ab90b6f0715
Wictorgirardi <wictor.girardi@gmail.com>
2022-12-21 01:05:25 -0300

    wip

--- src/servers/distribuido/server.py
+++ src/servers/distribuido/server.py
@@ -5,6 +5,33 @@ import RPi.GPIO as GPIO
 import socket
 import json
 
+def readConfig():
+    configfile = open('../../configs/configuracao_sala_02.json')
+    obj = json.load(configfile)
+    config = {}
+
+    config['ip_servidor_central'] = obj['ip_servidor_central']
+    config['porta_servidor_central'] = obj['porta_servidor_central']
+    config['ip_servidor_distribuido'] = obj['ip_servidor_distribuido']
+    config['porta_servidor_distribuido'] = obj['porta_servidor_distribuido']
+    config['nome'] = obj['nome']
+
+    config['L_01'] = obj['outputs'][0]['gpio']
+    config['L_02'] = obj['outputs'][1]['gpio']
+    config['PR'] = obj['outputs'][2]['gpio']
+    config['AC'] = obj['outputs'][3]['gpio']
+    config['AL_BZ'] = obj['outputs'][4]['gpio']
+
+    config['SPres'] = obj['inputs'][0]['gpio']
+    config['SFum'] = obj['inputs'][1]['gpio']
+    config['SJan'] = obj['inputs'][2]['gpio']
+    config['SPor'] = obj['inputs'][3]['gpio']
+    config['SC_IN'] = obj['inputs'][4]['gpio']
+    config['SC_OUT'] = obj['inputs'][5]['gpio']
+    config['DHT22'] = obj['sensor_temperatura'][0]['gpio']
+
+    return config
+
 def openSocket():
   configfile = open('../../configs/configuracao_sala_02.json')
   file = json.load(configfile)
@@ -12,9 +39,8 @@ def openSocket():
         server.connect((file['ip_servidor_central'], file['porta_servidor_central']))
   return server
   
-def receive(server):
-    configfile = open('../../configs/configuracao_sala_02.json')
-    file = json.load(configfile)
+
+def receive(server, config):
     while True:
       message = server.recv(2048).decode('ascii')
       print (message)
@@ -26,21 +52,21 @@ def receive(server):
 
       if message.startswith('ON_OFF_'):
         device = message[7:]
-        if GPIO.input(file[device]):
-          GPIO.output(file[device], GPIO.LOW)
+        if GPIO.input(config[device]):
+          GPIO.output(config[device], GPIO.LOW)
           server.send('OK'.encode('ascii'))
           continue
         else: 
-          GPIO.output(file[device], GPIO.HIGH)
+          GPIO.output(config[device], GPIO.HIGH)
           server.send('OK'.encode('ascii'))
           continue
 
       if message.startswith('ON_ALL'):
         try:
-          keys = [*file]
+          keys = [*config]
           for device in keys[5:10]:
-            print(file[device])
-            GPIO.output(file[device], GPIO.HIGH)
+            print(config[device])
+            GPIO.output(config[device], GPIO.HIGH)
           server.send('OK'.encode('ascii'))
           continue
         except: 
@@ -48,9 +74,9 @@ def receive(server):
 
       if message.startswith('OFF_ALL'):
         try:
-          keys = [*file]
+          keys = [*config]
           for device in keys[5:10]:
-            GPIO.output(file[device], GPIO.LOW)
+            GPIO.output(config[device], GPIO.LOW)
           server.send('OK'.encode('ascii'))
           continue
         except:
@@ -59,10 +85,11 @@ def receive(server):
 if __name__ == '__main__':
   try:
     server = openSocket()
-    controlThread = threading.Thread(target=control.states)
+    config = readConfig()
+    controlThread = threading.Thread(target=control.states, args=(config,))
     controlThread.start()
     print('Servidor inicializado, recebendo....')
-    receive(server) 
+    receive(server, config) 
 
   except KeyboardInterrupt: 
     exit()
