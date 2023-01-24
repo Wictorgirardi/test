9ed9df941fc2e5732a2516661a3fc75cf61f1726
Wictorgirardi <wictor.girardi@gmail.com>
2023-01-23 23:50:50 -0300

    wip

--- inc/variables.h
+++ inc/variables.h
@@ -3,22 +3,27 @@
 #ifndef VARIABLES_H_
 #define VARIABLES_H_
 
-#define uart0_filestream;
-#define internalTemp 0;
-#define externalTemp 0;
-#define userTemp  0;
-#define valueFan  0;
-#define valueResistor 0;
-#define millisAux  0;
-long millisCounter  = 0;
-#define systemState  0;
-#define funcState  0;
-#define modeState  0;
-#define menuChoice  0;
+
+int uart0_filestream;
+float internalTemp = 0;
+float externalTemp = 0;
+float userTemp = 0;
+int valueFan = 0;
+int valueResistor = 0;
+long millisAux = 0;
+long millisCounter = 0;
+// 1 ligado 0 desligado
+int systemState = 0;
+// 1 funcionando 0 parado
+int funcState = 0;
+// 1 curva/terminal 0 dashboard
+int modeState = 0;
+int menuChoice = 0;
 float ki = 0;
 float kp = 0;
 float kd = 0;
 pthread_t ovenThread;
 pthread_t reportThread;
+struct bme280_dev bme;
 
 #endif /* VARIABLES_H_ */
